f995170f835c54c8e4f0dba9126813a2838c0ff7
Jess <jessica.witor18@bathspa.ac.uk>
2021-01-09 14:13:31 +0000

    set up timer script and basic gametest script

--- Assets/Scripts/GameTest.cs
+++ Assets/Scripts/GameTest.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameTest : MonoBehaviour
 {
+    public GameObject crackerL;
+    public GameObject crackerR;
+    public float lStrength;
+    private float rStrength;
+    private Timer timer;
+    private bool timeUp = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = gameObject.GetComponent<Timer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timeUp == false && Input.GetKeyDown("space")) { lStrength += 1; } //increment strength every keypress
     }
+    void TimeUp() 
+    {
+        timeUp = true;
+        if (lStrength >= rStrength) {}
+    }
+
+    
 }
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
+    public GameObject target;//target gameobject to send message to
+    public float timer;
+    private float currentTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = timer;
     }
-
+    private void OnEnable()
+    {
+        currentTime = timer;
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void TimerFunc() 
+    {
+        currentTime -= 1 * Time.deltaTime;
+        if (currentTime <= 0) { target.SendMessage("TimeUp"); }
     }
 }
